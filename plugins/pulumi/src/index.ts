f1dd4475458354a1d7221cd683548a524e70115d
bitspire-ci <56037193+bitspire-ci@users.noreply.github.com>
2022-08-22 00:16:59 +0000

    Init sample pulumi plugin

---
+++ plugins/pulumi/src/index.ts
@@ -0,0 +1,1 @@
+export { pulumiPlugin, PulumiPage } from './plugin';
---
+++ plugins/pulumi/src/plugin.ts
@@ -0,0 +1,19 @@
+import { createPlugin, createRoutableExtension } from '@backstage/core-plugin-api';
+
+import { rootRouteRef } from './routes';
+
+export const pulumiPlugin = createPlugin({
+  id: 'pulumi',
+  routes: {
+    root: rootRouteRef,
+  },
+});
+
+export const PulumiPage = pulumiPlugin.provide(
+  createRoutableExtension({
+    name: 'PulumiPage',
+    component: () =>
+      import('./components/ExampleComponent').then(m => m.ExampleComponent),
+    mountPoint: rootRouteRef,
+  }),
+);
